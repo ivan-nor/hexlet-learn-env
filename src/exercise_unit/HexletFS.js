d4158b07f482edc58293b2e44a736550b73fca66
Ivan Nor <ivan.norik@gmail.com>
2019-10-11 18:14:00 +0300

    step 1

--- src/exercise_unit/HexletFS.js
+++ src/exercise_unit/HexletFS.js
@@ -1,30 +1,53 @@
 import path from 'path';
 import Tree from '@hexlet/trees';
 
-import Dir from './Dir';
-import File from './File';
-
-const getPathParts = filepath => filepath.split(path.sep).filter(part => part !== '');
+// BEGIN (write your solution here)
+const getPathParts = (strPath) => {
+  const normalizedPath = path.normalize(strPath);
+  return normalizedPath.split(path.sep);
+};
+// END
 
 export default class {
   constructor() {
-    this.tree = new Tree('/', new Dir('/'));
+    this.tree = new Tree('/', { type: 'dir' });
   }
 
-  statSync(filepath) {
-    const current = this.findNode(filepath);
-    return current.getMeta().getStats();
+  // BEGIN (write your solution here)
+  isDirectory(dirPath) {
+    // console.log(dirPath);
+    const normalizedPath = getPathParts(dirPath);
+    // console.log(normalizedPath);
+    const node = this.getDeepChild(normalizedPath);
+    const meta = node.getMeta();
+    return meta.type === 'dir';
   }
 
-  touchSync(filepath) {
-    const { dir, base } = path.parse(filepath);
-    return this.findNode(dir).addChild(base, new File(base));
+  isFile(filePath) {
+    console.log(filePath);
+    const normalizedPath = getPathParts(filePath);
+    console.log(normalizedPath);
+    const node = this.getDeepChild(normalizedPath);
+    const meta = node.getMeta();
+    return meta.type === 'file';
   }
 
-  mkdirSync(filepath) {
-    const { dir, base } = path.parse(filepath);
-    return this.findNode(dir).addChild(base, new Dir(base));
+  mkdirSync(dirPath) {
+    const normalizedPath = getPathParts(dirPath);
+    const parsingPath = path.parse(normalizedPath);
+    const node = this.getDeepChild(parsingPath.dir);
+    if (!node) return false;
+    return node.addChild(parsingPath.base, { type: 'dir' });
   }
+
+  touchSync(filePath) {
+    const normalizedPath = getPathParts(filePath);
+    const parsingPath = path.parse(normalizedPath);
+    const node = this.getDeepChild(parsingPath.dir);
+    if (!node) return false;
+    return node.addChild(parsingPath.base, { type: 'file' });
+  }
+  // END
 
   findNode(filepath) {
     const parts = getPathParts(filepath);
